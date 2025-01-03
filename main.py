d520559a89baaaa5ee996081b85a624964388461
vike256 <69812383+vike256@users.noreply.github.com>
2025-01-03 20:31:23 +0200

    Add comments

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 import concurrent.futures
 from time import time
 
+
 def parse_arguments():
     parser = argparse.ArgumentParser(
         prog='Domain Finder by vike256',
@@ -16,6 +17,10 @@ def parse_arguments():
     return parser.parse_args()
      
 
+'''
+Run a whois request, return True if "not found" is included in the response
+Also returns the domain to make parallel processing easier to implement
+'''
 def is_domain_available(domain):
     try:
         whois = subprocess.run(['whois', domain], stdout=subprocess.PIPE, timeout=10).stdout.decode('utf-8').lower()
@@ -29,6 +34,9 @@ def is_domain_available(domain):
     return domain, False
 
 
+'''
+Opens a namefile and returns the list of names
+'''
 def process_namefile(path):
     try:
         with open(path, 'r') as file:
@@ -42,24 +50,29 @@ def process_namefile(path):
 
 
 def main():
-    start_time = time()
+    start_time = time()     # To capture how long the program takes
     args = parse_arguments()
-    names = []
-    tlds = []
-    available = []
-    checked_count = 0
+    names = []              # Initialize the list of domain names to check
+    tlds = []               # Initialize the list of TLDs to check
+    available = []          # Initialize the list of available domains found
+    checked_count = 0       # Keep track of how many domains are checked
 
+    # Exit the program if there are no names to check
     if not args.name and not args.namefile:
         print('Please provide -name or -namefile') 
         print('-h for help')
         sys.exit()
     
+    # Add names from -name to namelist if found
     if args.name:
         names.extend(args.name)
     
+    # Add names from namefile to namelist if found
     if args.namefile:
         names.extend(process_namefile(args.namefile[0]))
     
+    # If user provides TLDs, only add those to the list
+    # Else add all TLDs that are provided with the program
     if args.tld:
         tlds.extend(args.tld)
     else:
@@ -73,7 +86,7 @@ def main():
                 else:
                     tlds.append(tld)
 
-    with open('available_domains.txt', 'w') as output_file:
+    with open('available_domains.txt', 'w') as output_file:     # Add available domains to a text-file
         with concurrent.futures.ThreadPoolExecutor() as executor:
             futures = []
             for name in names:
@@ -88,9 +101,15 @@ def main():
                 else:
                     print(f'{result_domain}, not available')
                 checked_count += 1
+
+        # Sort the list alphabetically
         available.sort()
+
+        # Add all available domains to a string and write it to a file
         output_string = ''.join(f'{d}\n' for d in available)
         output_file.write(output_string)
+
+        # Print stats
         found = len(available)
         print(f'Finished in {time() - start_time:.2f} seconds')
         print(f'Checked {checked_count} domains')
